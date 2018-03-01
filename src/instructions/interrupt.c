0a5e85b2fdddc596e929dcead69c8d008381dce8
SakiiR SakiiR <sakiirlessons@gmail.com>
2018-03-01 12:18:40 +0100

    EI - DI handlers

--- src/instructions/interrupt.c
+++ src/instructions/interrupt.c
@@ -1,19 +1,18 @@
 #include "resource.h"
 #include "cpu.h"
 #include "game.h"
+#include "interrupts.h"
 #include "memory.h"
 #include "memory_ar.h"
 
 int             i_di(t_game *game)
 {
-  (void)game;
-  /* TODO: Disable interrupt */
+  game->interrupts.enabled = INTERRUPTS_DISABLED;
   return RETURN_SUCCESS;
 }
 
 int             i_ei(t_game *game)
 {
-  (void)game;
-  /* TODO: Enable interrupts */
+  game->interrupts.enabled = INTERRUPTS_ENABLED;
   return RETURN_SUCCESS;
 }
