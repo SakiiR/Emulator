c217b65ff2537ce9a69b5a33ad543ba5d6fce69d
SakiiR SakiiR <sakiirlessons@gmail.com>
2018-03-04 15:58:20 +0100

    Wait before implementing timing

--- src/timing.c
+++ src/timing.c
@@ -1,4 +1,3 @@
-#include <time.h>
 #include <stdio.h>
 #include "resource.h"
 #include "timer.h"
@@ -9,14 +8,6 @@
 
 int                 timing_step(t_game *game)
 {
-  struct timespec   res = {0, 0};
-
-  clock_gettime(CLOCK_MONOTONIC_RAW, &res);
-  printf("Time: %lu\n", res.tv_nsec);
-  game->timer.cycles += game->state.instruction->cycle_count;
-  if (game->timer.cycles > MAX_CYCLE_TO_REFRESH)
-  {
-    
-  }
+  (void)game;
   return RETURN_SUCCESS;
 }
